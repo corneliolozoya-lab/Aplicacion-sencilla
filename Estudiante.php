1fedb904d332260adaf917b75a95163aecbd593d
corneliolozoya-lab <corneliolozoya@gmail.com>
2026-03-17 22:59:01 -0600

    Update Estudiante.php

--- Estudiante.php
+++ Estudiante.php
@@ -1,11 +1,11 @@
 <?php
-// Clase Estudiante
+// Clase base Estudiante
 class Estudiante {
 
-    // Atributos
-    private $nombre;
-    private $matricula;
-    private $carrera;
+    // Atributos protegidos para permitir herencia
+    protected $nombre;
+    protected $matricula;
+    protected $carrera;
 
     // Constructor
     public function __construct($nombre, $matricula, $carrera) {
@@ -14,6 +14,14 @@ class Estudiante {
         $this->carrera = $carrera;
     }
 
+    // Método para mostrar datos
+    public function mostrarDatos() {
+        return "Nombre: {$this->nombre} <br>
+                Matrícula: {$this->matricula} <br>
+                Carrera: {$this->carrera} <br>";
+    }
+}
+?>
     // Método para mostrar información
     public function mostrarDatos() {
         return "Nombre: $this->nombre <br>
